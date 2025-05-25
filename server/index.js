1dedab5e1873fbd77aae7aebe1b5d454521d9648
SohelShahid117 <sohelshahid09@gmail.com>
2025-05-25 23:58:06 +0600

    library management system  40 min done

--- server/index.js
+++ server/index.js
@@ -2,6 +2,37 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+//connect with mongodb
+
+const uri =
+  "mongodb+srv://sohelshahid09:5QYJoCjMgHeicc1T@library-management-syst.88n1mrb.mongodb.net/?retryWrites=true&w=majority&appName=library-management-system";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Welcome to Book Management API");
 });
@@ -9,3 +40,8 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+//mongodb userId + pw
+//sohelshahid09
+//5QYJoCjMgHeicc1T
+/*mongodb+srv://sohelshahid09:5QYJoCjMgHeicc1T@library-management-syst.88n1mrb.mongodb.net/?retryWrites=true&w=majority&appName=library-management-system*/
